5abd2869fd28c2ba60ceab5004ceed446f08bbc5
unknown <vuhuyanhnew@gmail.com>
2023-09-19 19:45:06 +0700

    update BT

--- Advance_JS/BT/BT2/script.js
+++ Advance_JS/BT/BT2/script.js
@@ -13,9 +13,30 @@ class ToDoList {
     constructor() {
         this.toDos = [];
     }
-} 
+
+    addToDo(toDo) {
+        const existingToDo = this.toDos.find(item => item.date == toDo.date);
+
+        if (existingToDo) {
+           console.log("Existed work!")
+        } else {
+            this.toDos.push(toDo);
+        }
+    }
+}
 const todoList = new ToDoList();
 
 const todo1 = new ToDo("2023-09-15");
 todo1.addWork("work A");
 todo1.addWork("work B");
+
+const todo2 = new ToDo("2023-09-16");
+todo1.addWork("work C");
+todo1.addWork("work E");
+
+const todo3 = new ToDo("2023-09-16");
+todo1.addWork("work F");
+todo1.addWork("work G");
+todoList.addToDo(todo1)
+todoList.addToDo(todo2)
+console.log(todoList)
--- async/BT/index.js
+++ async/BT/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 async function fetch() {
-    const result = await axios.get("https://pokeapi.co/");
+    const result = await axios.get("https://pokeapi.co/api/v2/pokemon");
     console.log(result.data)
 }
 fetch();
